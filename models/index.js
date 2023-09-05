38f1abdd8e810677fa5fec8cf10a25ba98cf65a7
cumauu73 <cumauu73@icloud.com>
2023-09-05 18:25:00 -0500

    index.js product category and tags

--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  forignKey: 'category_id'
+})
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  forignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
